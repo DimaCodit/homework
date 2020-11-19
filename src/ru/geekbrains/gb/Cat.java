01f75ea2e3420a09477711dfcb2cac458859d219
Dima Rakhimov <rakhimov9@gmail.com>
2020-11-20 00:04:11 +0500

    Правки к ДЗ 6. Запретить коту плавать

--- src/ru/geekbrains/gb/Cat.java
+++ src/ru/geekbrains/gb/Cat.java
@@ -3,6 +3,11 @@ package ru.geekbrains.gb;
 public class Cat extends Animal{
 
     public Cat(int swimmingLimit, int runningLimit, double jumpingLimit) {
-        super(swimmingLimit, runningLimit, jumpingLimit);
+        super(0, runningLimit, jumpingLimit);
+    }
+
+    @Override
+    void swim(int actionMeasure) {
+        System.out.println("cat do not can swim");
     }
 }
